c7ed1ede8029517c49d8aec61ad5051e25959538
RTimmerGH <timmer.romain@gmail.com>
2023-10-10 11:43:14 +0200

    Spring - IoC & SI

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -1,7 +1,7 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.Wizard;
-import com.wildcodeschool.wildandwizard.repository.WizardRepository;
+import com.wildcodeschool.wildandwizard.repository.WizardDao;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WizardController {
 
-    private WizardRepository repository = new WizardRepository();
+    private final WizardDao repository;
+
+    public WizardController(
+            WizardDao repositoryInjected
+    ) {
+        this.repository = repositoryInjected;
+    }
 
     @GetMapping("/wizards")
     public String getAll(Model model) {
